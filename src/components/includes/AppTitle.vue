22da1ae28383f1a50896cd00ca8335ae51a20af7
noval <novalmaulana@gmail.com>
2020-04-25 12:10:47 +0700

    use props

--- src/components/includes/AppTitle.vue
+++ src/components/includes/AppTitle.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="app-title">
     <div>
-      <h1><i class="fa" :class="icon"></i> {{ title }}</h1>
-      <p>{{description}}</p>
+      <h1><i class="fa" :class="props.icon"></i> {{ props.title }}</h1>
+      <p>{{ props.description }}</p>
     </div>
     <ul class="app-breadcrumb breadcrumb">
       <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
-      <li class="breadcrumb-item" v-for="(b, index) in breadcrumb" :key="index">
+      <li class="breadcrumb-item" v-for="(b, index) in props.breadcrumb" :key="index">
         <a href="#">{{ b }}</a>
       </li>
     </ul>
@@ -17,10 +17,5 @@
 export default {
   name: 'AppTitle',
   props: ['props'],
-  data() {
-    return {
-      ...this.props
-    }
-  }
 }
 </script>
