5420099f08e938292a8583a8318b96e459b06cf5
noval <novalmaulana@gmail.com>
2020-04-25 12:00:56 +0700

    sidebaritem component

--- src/components/includes/Sidebar.vue
+++ src/components/includes/Sidebar.vue
@@ -7,50 +7,138 @@
       </div>
     </div>
     <ul class="app-menu">
-      <li><a class="app-menu__item" href="dashboard.html"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Dashboard</span></a></li>
-      <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-laptop"></i><span class="app-menu__label">UI Elements</span><i class="treeview-indicator fa fa-angle-right"></i></a>
-        <ul class="treeview-menu">
-          <li><a class="treeview-item" href="bootstrap-components.html"><i class="icon fa fa-circle-o"></i> Bootstrap Elements</a></li>
-          <li><a class="treeview-item" href="https://fontawesome.com/v4.7.0/icons/" target="_blank" rel="noopener"><i class="icon fa fa-circle-o"></i> Font Icons</a></li>
-          <li><a class="treeview-item" href="ui-cards.html"><i class="icon fa fa-circle-o"></i> Cards</a></li>
-          <li><a class="treeview-item" href="widgets.html"><i class="icon fa fa-circle-o"></i> Widgets</a></li>
-        </ul>
-      </li>
-      <li><a class="app-menu__item" href="charts.html"><i class="app-menu__icon fa fa-pie-chart"></i><span class="app-menu__label">Charts</span></a></li>
-      <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-edit"></i><span class="app-menu__label">Forms</span><i class="treeview-indicator fa fa-angle-right"></i></a>
-        <ul class="treeview-menu">
-          <li><a class="treeview-item" href="form-components.html"><i class="icon fa fa-circle-o"></i> Form Components</a></li>
-          <li><a class="treeview-item" href="form-custom.html"><i class="icon fa fa-circle-o"></i> Custom Components</a></li>
-          <li><a class="treeview-item" href="form-samples.html"><i class="icon fa fa-circle-o"></i> Form Samples</a></li>
-          <li><a class="treeview-item" href="form-notifications.html"><i class="icon fa fa-circle-o"></i> Form Notifications</a></li>
-        </ul>
-      </li>
-      <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-th-list"></i><span class="app-menu__label">Tables</span><i class="treeview-indicator fa fa-angle-right"></i></a>
-        <ul class="treeview-menu">
-          <li><a class="treeview-item" href="table-basic.html"><i class="icon fa fa-circle-o"></i> Basic Tables</a></li>
-          <li><a class="treeview-item" href="table-data-table.html"><i class="icon fa fa-circle-o"></i> Data Tables</a></li>
-        </ul>
-      </li>
-      <li class="treeview is-expanded"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-file-text"></i><span class="app-menu__label">Pages</span><i class="treeview-indicator fa fa-angle-right"></i></a>
-        <ul class="treeview-menu">
-          <li><a class="treeview-item active" href="blank-page.html"><i class="icon fa fa-circle-o"></i> Blank Page</a></li>
-          <li><a class="treeview-item" href="page-login.html"><i class="icon fa fa-circle-o"></i> Login Page</a></li>
-          <li><a class="treeview-item" href="page-lockscreen.html"><i class="icon fa fa-circle-o"></i> Lockscreen Page</a></li>
-          <li><a class="treeview-item" href="page-user.html"><i class="icon fa fa-circle-o"></i> User Page</a></li>
-          <li><a class="treeview-item" href="page-invoice.html"><i class="icon fa fa-circle-o"></i> Invoice Page</a></li>
-          <li><a class="treeview-item" href="page-calendar.html"><i class="icon fa fa-circle-o"></i> Calendar Page</a></li>
-          <li><a class="treeview-item" href="page-mailbox.html"><i class="icon fa fa-circle-o"></i> Mailbox</a></li>
-          <li><a class="treeview-item" href="page-error.html"><i class="icon fa fa-circle-o"></i> Error Page</a></li>
-        </ul>
-      </li>
-      <li><a class="app-menu__item" href="docs.html"><i class="app-menu__icon fa fa-file-code-o"></i><span class="app-menu__label">Docs</span></a></li>
+      <SidebarItem v-for="(item, index) in sidebarItems" :key="index" :item="item" />
     </ul>
   </aside>
 </template>
 
 <script>
+import SidebarItem from './SidebarItem'
 export default {
   name: 'Sidebar',
-  
+  data() {
+    return {
+      sidebarItems: [
+        {
+          to: '/dashboard',
+          label: 'Dashboard',
+          icon: 'fa-dashboard'
+        },
+        {
+          to: '#',
+          label: 'UI-Elements',
+          icon: 'fa-laptop',
+          children: [
+            {
+              to: '',
+              label: 'Bootstrap Elements'
+            },
+            {
+              to: '',
+              label: 'Font Icons'
+            },
+            {
+              to: '',
+              label: 'Cards'
+            },
+            {
+              to: '',
+              label: 'Widgets'
+            },
+          ]
+        },
+        {
+          to: 'charts',
+          label: 'Charts',
+          icon: 'fa-pie-chart'
+        },
+        {
+          to: '#',
+          label: 'Forms',
+          icon: 'fa-edit',
+          children: [
+            {
+              to: 'form-components',
+              label: 'Form Components'
+            },
+            {
+              to: 'form-custom',
+              label: 'Custom Components'
+            },
+            {
+              to: 'form-samples',
+              label: 'Form Samples'
+            },
+            {
+              to: 'form-notifications',
+              label: 'Form Notifications'
+            },
+          ]
+        },
+        {
+          to: '#',
+          label: 'Tables',
+          icon: 'fa-th-list',
+          children: [
+            {
+              to: 'table-basic',
+              label: 'Basic Tables'
+            },
+            {
+              to: 'table-data-table',
+              label: 'Data Tables'
+            },
+          ]
+        },
+        {
+          to: '#',
+          label: 'Pages',
+          icon: 'fa-file-text',
+          children: [
+            {
+              to: 'blank-page',
+              label: 'Blank Page'
+            },
+            {
+              to: 'page-login',
+              label: 'Login Page'
+            },
+            {
+              to: 'page-lockscreen',
+              label: 'Lockscreen Page'
+            },
+            {
+              to: 'page-user',
+              label: 'User Page'
+            },
+            {
+              to: 'page-invoice',
+              label: 'Invoice Page'
+            },
+            {
+              to: 'page-calendar',
+              label: 'Calendar Page'
+            },
+            {
+              to: 'page-mailbox',
+              label: 'Mailbox Page'
+            },
+            {
+              to: 'page-error',
+              label: 'Error Page'
+            },
+          ]
+        },
+        {
+          to: 'docs',
+          label: 'Docs',
+          icon: 'fa-file-code-o'
+        }
+      ]
+    }
+  },
+  components: {
+    SidebarItem
+  }
 }
 </script>
---
+++ src/components/includes/SidebarItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <li v-if="!item.children">
+      <router-link class="app-menu__item" :to="item.to">
+        <i class="app-menu__icon fa" :class="item.icon"></i>
+        <span class="app-menu__label">{{ item.label }}</span> 
+      </router-link>
+    </li>
+    <li v-else class="treeview" :class="{ 'is-expanded': isExpanded }" @click="expandMenu">
+      <router-link class="app-menu__item" :to="item.to">
+        <i class="app-menu__icon fa" :class="item.icon"></i>
+        <span class="app-menu__label">{{ item.label }}</span> 
+        <i v-if="item.children && item.children.length > 0" class="treeview-indicator fa fa-angle-right"></i>
+      </router-link>
+      <ul class="treeview-menu" v-if="item.children && item.children.length > 0">
+        <li v-for="(child, index) in item.children" :key="index" class="">
+          <router-link class="treeview-item" :to="child.to">
+            <i class="icon fa fa-circle-o"></i> {{ child.label }}
+          </router-link>
+        </li>
+      </ul>
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SidebarItem',
+  props: ['item'],
+  data() {
+    return {
+      isExpanded: false
+    }
+  },
+  methods: {
+    expandMenu() {
+      this.isExpanded = !this.isExpanded
+    }
+  }
+}
+</script>
