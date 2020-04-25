e0d55c107455ac48aa4cd46a46dba09391b352a3
noval <novalmaulana@gmail.com>
2020-04-25 12:15:24 +0700

    desctructuring props

--- src/components/includes/AppTitle.vue
+++ src/components/includes/AppTitle.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="app-title">
     <div>
-      <h1><i class="fa" :class="props.icon"></i> {{ props.title }}</h1>
-      <p>{{ props.description }}</p>
+      <h1><i class="fa" :class="icon"></i> {{ title }}</h1>
+      <p>{{ description }}</p>
     </div>
     <ul class="app-breadcrumb breadcrumb">
       <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
-      <li class="breadcrumb-item" v-for="(b, index) in props.breadcrumb" :key="index">
+      <li class="breadcrumb-item" v-for="(b, index) in breadcrumb" :key="index">
         <a href="#">{{ b }}</a>
       </li>
     </ul>
@@ -16,6 +16,11 @@
 <script>
 export default {
   name: 'AppTitle',
-  props: ['props'],
+  props: {
+    title: String,
+    icon: String,
+    description: String,
+    breadcrumb: Array,
+  },
 }
 </script>
--- src/components/pages/Dashboard.vue
+++ src/components/pages/Dashboard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app-content">
-    <app-title :props="appTitle"></app-title>
+    <app-title v-bind="appTitle"></app-title>
   </div>
 </template>
 
